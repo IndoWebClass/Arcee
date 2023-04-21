17704ab74d84bf7e749ec408f00e41d1ff342add
IndoWebClass <IndoWebClass@gmail.com>
2023-04-21 20:23:30 +0700

    update template class

--- core/_template.php
+++ core/_template.php
@@ -3,7 +3,29 @@ namespace app\core;
 
 class ClassName
 {
+    protected Application $app;
     public function __construct(array $params)
     {
+        $this->app = Application::$app;
+
+
+        $this->init();
     }
+
+    //init
+        protected function init()
+        {
+
+        }
+    //init
+
+
+    //set variable
+    //set variable
+
+    //get / return variable
+    //get / return variable
+
+    //data proses
+    //data proses
 }
