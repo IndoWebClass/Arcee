b66de5873c591e76b6040ca09d5abec5073f27bf
IndoWebClass <IndoWebClass@gmail.com>
2023-05-01 19:52:42 +0700

    getPageId

---
+++ core/Ajax.php
@@ -0,0 +1,43 @@
+<?php
+namespace app\core;
+
+class Ajax
+{
+    protected Application $app;
+    public function __construct(array $params= [])
+    {
+        $this->app = Application::$app;
+
+
+        $this->init();
+    }
+
+    //init
+        protected function init()
+        {
+
+        }
+    //init
+
+
+    //set variable
+    //set variable
+
+    //get / return variable
+        public function getPageId()
+        {
+            $scriptName = $_SERVER["SCRIPT_NAME"];
+            $explode = explode("/",$scriptName);
+
+            $folder = $explode[3];
+            $pageId = intval(substr($folder,0,3));
+
+            return $pageId;
+
+
+        }
+    //get / return variable
+
+    //data proses
+    //data proses
+}
